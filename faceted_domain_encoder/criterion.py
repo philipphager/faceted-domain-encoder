c30470280ea9a0b016271b894090248192a7b828
Philipp Hager <philipp.hager@student.hpi.uni-potsdam.de>
2020-06-25 22:46:51 +0200

    Modify Loss

--- faceted_domain_encoder/criterion.py
+++ faceted_domain_encoder/criterion.py
@@ -1,17 +1,15 @@
+import torch
 from torch import nn
 
 
 class MaskedMSELoss(nn.Module):
-    def __init__(self, category_missing_value=1, mask=True):
+    def __init__(self, category_missing_value=1):
         super().__init__()
         self.category_missing_value = category_missing_value
-        self.mask = mask
 
     def forward(self, y_predict, y):
-        mean_absolute_error = y - y_predict
-
-        if self.mask:
-            category_mask = (y != self.category_missing_value).bool()
-            mean_absolute_error = mean_absolute_error * category_mask
-
-        return (mean_absolute_error ** 2).mean()
+        category_mask = (y != self.category_missing_value).bool()
+        mse = (y - y_predict) ** 2
+        masked_mse = mse * category_mask
+        error = masked_mse.sum(1) / torch.clamp(masked_mse.sum(1), min=1)
+        return error.mean()
