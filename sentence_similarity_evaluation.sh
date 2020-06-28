6dda72964c271129513f71d4ddcbe15d5bcf3028
Philipp Hager <philipp.hager@student.hpi.uni-potsdam.de>
2020-06-28 18:35:21 +0200

    Update STS benchmark

--- sentence_similarity_evaluation.sh
+++ sentence_similarity_evaluation.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 poetry shell
 
+# BIOSSES Baseline
+#python experiments/sentence_similarity/biosses_baseline.py
+
+# BIOSSES
+python experiments/sentence_similarity/biosses.py -m \
+  encoder=gru,lstm,transformer \
+  normalizer=corpus,document,pass \
+  pooling=category_attention,self_attention \
+
+
 # MedSTS Baseline
 python experiments/sentence_similarity/medsts_baseline.py
 
 # MedSTS
 python experiments/sentence_similarity/medsts.py -m \
-  encoder=gru,lstm,transformer \
-  normalizer=corpus,document,pass \
-  pooling=category_attention,self_attention \
-
-# MedSTS Baseline
-python experiments/sentence_similarity/medsts_baseline.py
-
-# BIOSSES
-python experiments/sentence_similarity/biosses.py -m \
   encoder=gru,lstm,transformer \
   normalizer=corpus,document,pass \
   pooling=category_attention,self_attention \
